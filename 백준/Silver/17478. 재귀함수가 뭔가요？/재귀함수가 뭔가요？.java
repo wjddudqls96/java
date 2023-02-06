2972c94bc2be584ca86e2e336f49b27e39561006
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-02-06 15:20:56 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 128 ms, Memory: 14400 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.java
@@ -8,31 +8,35 @@ import java.util.Set;
 public class Main {
 	static int N;
 	static String tab = "____";
+	static StringBuilder result = new StringBuilder();
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
 		N = Integer.parseInt(in.readLine());
-		System.out.println("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.");
+		result.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
 		cycle(0);
 		
+		System.out.println(result);
 	}
 	
 	static void cycle(int count) {
 		StringBuilder sb = new StringBuilder();
+		
 		for(int i = 0; i < count; i++) {
 			sb.append(tab);
 		}
+		
 		if(count == N) {
-			System.out.println(sb + "\"재귀함수가 뭔가요?\"");
-			System.out.println(sb + "\"재귀함수는 자기 자신을 호출하는 함수라네\"");
-			System.out.println(sb + "라고 답변하였지.");
+			result.append(sb).append("\"재귀함수가 뭔가요?\"").append("\n");
+			result.append(sb).append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+			result.append(sb).append("라고 답변하였지.").append("\n");
 			return;
 		}
-		System.out.println(sb + "\"재귀함수가 뭔가요?\"");
-		System.out.println(sb + "\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.");
-		System.out.println(sb + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.");
-		System.out.println(sb + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"");
+		result.append(sb).append("\"재귀함수가 뭔가요?\"").append("\n");
+		result.append(sb).append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n");
+		result.append(sb).append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n");
+		result.append(sb).append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
 		cycle(count + 1);
-		System.out.println(sb + "라고 답변하였지.");
+		result.append(sb).append("라고 답변하였지.").append("\n");
 	}
 }
