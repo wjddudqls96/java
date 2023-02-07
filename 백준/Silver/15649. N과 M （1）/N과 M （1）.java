2489222b04b72cafa7192dbe29e12612b9c16a67
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-02-07 13:42:13 +0900

    [Silver III] Title: N과 M (1), Time: 332 ms, Memory: 34732 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+	static int N, M;
+	static boolean[] isVisited;
+	static int[] numbers;
+	static int[] input;
+	static StringBuilder sb = new StringBuilder();
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		String[] line = in.readLine().split(" ");
+		
+		N = Integer.parseInt(line[0]);
+		M = Integer.parseInt(line[1]);
+		
+		isVisited = new boolean[N + 1];
+		numbers = new int[M];
+		
+		recursion(0);
+		
+		System.out.println(sb.toString());
+	}
+	
+	static void recursion(int num) {
+		if(num == M) {
+			for(int n : numbers) {
+				sb.append(n + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		
+		for(int i = 1; i <= N; i++) {
+			if(isVisited[i]) continue;
+			
+			numbers[num] = i;
+			isVisited[i] = true;
+			recursion(num + 1);
+			isVisited[i] = false;
+		}
+	}
+}
