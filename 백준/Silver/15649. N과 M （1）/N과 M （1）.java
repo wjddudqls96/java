94888a2c1382761f3b05c6ea337c9d75490dfdcd
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-02-07 13:43:13 +0900

    [Silver III] Title: N과 M (1), Time: 316 ms, Memory: 34464 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,15 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
 	static int N, M;
 	static boolean[] isVisited;
 	static int[] numbers;
-	static int[] input;
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
