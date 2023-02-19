89dbd8de307b4f204407eff3ba5f63784ca433ee
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-02-19 16:09:48 +0900

    [Silver I] Title: IOIOI, Time: 252 ms, Memory: 21992 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -9,23 +9,28 @@ public class Main {
 	static int N, M;
 	static String S;
 	static int[] numbers;
-	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(in.readLine());
 		M = Integer.parseInt(in.readLine());
 		S = in.readLine();
-		
-		String p = "IOI";
+		int pCnt = 0; 
 		int count = 0;
 		
-		for(int i = 1; i < N; i++) {
-			p += "OI";
-		}
+		char[] arr = S.toCharArray();
+
 		
-		for(int i = 0; i < S.length() - p.length(); i++) {
-			if(S.substring(i, i + p.length()).equals(p)) {
-				count++;
+		for(int i = 1; i < arr.length - 1; i++) {
+			if(arr[i - 1] == 'I' && arr[i] == 'O' && arr[i + 1] == 'I') {
+				pCnt++;
+				if(pCnt == N) {
+					pCnt--;
+					count++;
+				}
+				i++;
+			}
+			else {
+				pCnt = 0;
 			}
 		}
 		System.out.println(count);
