c0a94b5925d3a552ef97db594a02b1361cb43056
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-02-27 16:04:21 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 130 ms, Memory: 26,420 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.java
@@ -2,12 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Solution {
 	static int N, M;
-	static int[] deep;
-	static boolean[] visited;
+	static int[] parents;
 	static ArrayList<ArrayList<Integer>> matrix;
     public static void main(String[] args) throws Exception {
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
@@ -15,49 +16,62 @@ public class Solution {
         StringTokenizer st = null;
         
         int T = Integer.parseInt(in.readLine());
+        
         for(int t = 1; t <= T; t++) {
         	st = new StringTokenizer(in.readLine());
         	N = Integer.parseInt(st.nextToken());
         	M = Integer.parseInt(st.nextToken());
         	
-        	matrix = new ArrayList<>();
-        	for(int i = 0; i <= N; i++) {
-        		matrix.add(new ArrayList<>());
-        	}
-        	
-        	visited = new boolean[N + 1];
+        	parents = new int[N + 1];
+        	makeSet(N);
         	
         	for(int i = 0; i < M; i++) {
         		st = new StringTokenizer(in.readLine());
         		int a = Integer.parseInt(st.nextToken());
         		int b = Integer.parseInt(st.nextToken());
         		
-        		matrix.get(a).add(b);
-        		matrix.get(b).add(a);
+        		union(a, b);
         	}
         	
-        	int count = 0;
-        	for(int i = 1; i <= N; i++) {
-        		if(!visited[i]) {
-        			count++;
-        			dfs(i);        		
-        		}
+        	
+        	HashSet<Integer> set = new HashSet<>();
+        	
+        	for(int i = 1; i < parents.length; i++) {
+        		findSet(i);
         	}
         	
-        	sb.append("#" + t + " " + count).append("\n");
+        	for(int i = 1; i < parents.length; i++) {
+        		set.add(parents[i]);
+        	}
+        	
+        	sb.append("#" + t + " " + set.size()).append("\n"); 
         }
         System.out.println(sb);
     }
     
-    static void dfs(int index) {
-    	if(visited[index]) {
-    		return;
+    static void makeSet(int N) {
+    	for(int i = 1; i <= N; i++) {
+    		parents[i] = i;
+    	}
+    }
+    
+    static int findSet(int a) {
+    	if(parents[a] == a) {
+    		return a;
     	}
     	
-    	ArrayList<Integer> list = matrix.get(index);
-    	for(int i = 0; i < list.size(); i++) {
-    		visited[index] = true;
-    		dfs(list.get(i));
+    	return parents[a] = findSet(parents[a]);
+    }
+    
+    static boolean union(int a, int b) {
+    	int aRoot = findSet(a);
+    	int bRoot = findSet(b);
+    	
+    	if(aRoot == bRoot) {
+    		return false;
     	}
+    	
+    	parents[bRoot] = aRoot;
+    	return true;
     }
 }
