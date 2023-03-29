01ce1d76804965e01d26ecee0fe624a6ed010610
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-03-29 14:13:16 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 132 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.java
@@ -1,34 +1,21 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Queue;
+import java.util.LinkedHashMap;
 import java.util.StringTokenizer;
 
-class Pos{
-	int y, x, type;
-
-	public Pos(int x, int y, int type) {
-		super();
-		this.y = y;
-		this.x = x;
-		this.type = type;
-	}
-	
-}
-
-class Main {
-	static int N, count;
+public class Main {
+	static int N, max;
 	static int[][] map;
+	static boolean[][] visited;
+	static int[][] directions = {{1, -1},{1, 1},{-1, 1},{-1, -1}};
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = null;
-		StringBuilder sb = new StringBuilder();
 		
 		N = Integer.parseInt(in.readLine());
-		
 		map = new int[N][N];
 		
 		for(int i = 0; i < N; i++) {
@@ -38,78 +25,52 @@ class Main {
 			}
 		}
 		
-		bfs(1, 0);
+		int[][][] DP = new int[N][N][3];
 		
-		System.out.println(count);
-	}
-	
-	
-	static void bfs(int startX, int startY) {
-		int[][] direction = {{1, 0},{0, 1},{1, 1}};
-		Queue<Pos> queue = new ArrayDeque<>();
-		queue.offer(new Pos(startX, startY, 0));
+		if(map[0][2] != 1) {
+			DP[0][2][0] = 1;
+			DP[0][2][1] = 0;
+			DP[0][2][2] = 0;
+		}
 		
-		// 0 가로 1 세로 2 대각선
-		while(!queue.isEmpty()) {
-			Pos pos = queue.poll();
-			
-			if(pos.x == N - 1 && pos.y == N -1) {
-				count++;
+		if(map[1][2] != 1 && map[0][2] != 1 && map[1][1] != 1) {
+			DP[1][2][0] = 0;
+			DP[1][2][1] = 1;
+			DP[1][2][2] = 0;
+		}
+		
+		
+		for(int i = 0; i < N; i++) {
+			for(int j = 2; j < N; j++) {
+				if(map[i][j] == 1) continue;
+				
+				if(i == 1 && j == 2) continue;
+				// 가로로 왔을때
+				if(j - 1 >= 0) {
+					if(map[i][j - 1] != 1) {
+						DP[i][j][0] += DP[i][j - 1][0] + DP[i][j - 1][1];
+					}
+					
+				}
+				
+				// 대각으로 왔을때
+				if(j - 1 >= 0 && i - 1 >= 0) {
+					if(map[i - 1][j - 1] != 1 && map[i - 1][j] != 1 && map[i][j - 1] != 1) {
+						DP[i][j][1] = DP[i - 1][j - 1][0] + DP[i - 1][j - 1][1] + DP[i - 1][j - 1][2];
+					}
+					
+				}
+				
+				// 세로로 왔을때
+				if(i - 1 >= 0) {
+					if(map[i - 1][j] != 1) {
+						DP[i][j][2] = DP[i - 1][j][2] + DP[i - 1][j][1];
+					}
+					
+				}
 			}
-			
-			switch (pos.type) {
-				case 0:
-					goRight(queue, pos);
-					goDegak(queue, pos);
-					break;
-				case 1:
-					goDown(queue, pos);
-					goDegak(queue, pos);
-					break;
-				case 2:
-					goRight(queue, pos);
-					goDown(queue, pos);
-					goDegak(queue, pos);
-					break;
-			}
 		}
-	}
-	
-	static void goRight(Queue<Pos> queue, Pos pos) {
-		int nextX = pos.x + 1;
-		int nextY = pos.y;
 		
-		if(!isPossible(nextX, nextY)) return;
-		
-		if(map[nextY][nextX] != 1) {
-			queue.offer(new Pos(nextX, nextY, 0));
-		}
-	}
-	
-	static void goDown(Queue<Pos> queue, Pos pos) {
-		int nextX = pos.x;
-		int nextY = pos.y + 1;
-		
-		if(!isPossible(nextX, nextY)) return;
-		
-		if(map[nextY][nextX] != 1) {
-			queue.offer(new Pos(nextX, nextY, 1));
-		}
-	}
-	
-	static void goDegak(Queue<Pos> queue, Pos pos) {
-		int nextX = pos.x + 1;
-		int nextY = pos.y + 1;
-		
-		if(!isPossible(nextX, nextY)) return;
-		
-		if(map[nextY][nextX] != 1 && map[nextY - 1][nextX] != 1 && map[nextY][nextX - 1] != 1) {
-			queue.offer(new Pos(nextX, nextY, 2));
-		}
-	}
-	
-	static boolean isPossible(int x, int y) {
-		if(x < 0 || x >= N || y < 0 || y >= N) return false;
-		return true;
+		System.out.println(DP[N - 1][N - 1][0] + DP[N - 1][N - 1][1] + DP[N - 1][N - 1][2]);
 	}
 }
