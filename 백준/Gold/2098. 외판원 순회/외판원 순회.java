8c9ab5b21846e3559d9e7a4a7f6fd6f9a0a0235e
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-04-06 14:54:53 +0900

    [Gold I] Title: 외판원 순회, Time: 236 ms, Memory: 21468 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.java
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.java
@@ -1,53 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.HashMap;
+import java.util.StringTokenizer;
 
 public class Main {
-
-	static int N;
-	static int [][] dist;
-	static int [][] dp;
-	static int min;
-	static int INF=Integer.MAX_VALUE/100;
-	public static void main(String[] args) {
+	static int N, INF = Integer.MAX_VALUE/100;
+	static int[][] dist, dp;
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
 		
-		Scanner scann=new Scanner(System.in);
-		N=scann.nextInt();
-		dist=new int[N][N];
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j <N; j++) {
-				dist[i][j]=scann.nextInt();
+		N = Integer.parseInt(in.readLine());
+		dist = new int[N][N];
+		dp = new int[1 << N][N];
+		
+		for(int i = 0; i < N; i++) {
+			st = new StringTokenizer(in.readLine());
+			for(int j = 0; j < N; j++) {
+				dist[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		dp=new int[1<<N][N];
-		for (int i =0; i < 1<<N ; i++) {
+		
+		for(int i = 0; i < (1 << N); i++) {
 			Arrays.fill(dp[i], -1);
 		}
-		min=tsp(1,0);
-		System.out.println(min);
+		
+		System.out.println(tps(1, 0));
+		
 	}
-	private static int tsp(int visited,int city) {
-		// 모두 방문했니 그러면 마지막 도시와 집과의 거리는?
-		if(visited==((1<<N)-1)){ 
-			//만약 연결이 안되어 있다면 
-			if(dist[city][0]==0) return INF;
-			return dist[city][0];// 마지막 도시와 집까지 거리
+	
+	static int tps(int visited, int city) {
+		
+		// 마지막 도착지에 도착한다면
+		if(visited == ((1 << N) - 1)) {
+			// 마지막 도착지에서 처음으로 못되돌아간다면.
+			if(dist[city][0] == 0) return INF;
+			return dist[city][0];
 		}
-		// 도시 방문순서를 고려포함
-		// 방문도시 순서에(이 도시 방문하려고 함) 같은 순서가 있어
-		if(dp[visited][city]!=-1){
+		
+		
+		if(dp[visited][city] != -1) {
 			return dp[visited][city];
 		}
-		// 방문 표시
-		dp[visited][city]=INF;
-		for (int i = 0; i < N; i++) {
-			// 중복방문x
-			if((visited &(1<<i))!=0)continue;
-			// 도시가 연결안됨
-			if(dist[city][i]==0)continue;
+		
+		dp[visited][city] = INF;
+		
+		for(int i = 0; i < N; i++) {
+			if((visited & (1 << i)) != 0) continue;
 			
-			int res=tsp(visited |(1<<i),i)+dist[city][i];
-			dp[visited][city]=Math.min(res, dp[visited][city]);
+			if(dist[city][i] == 0) continue;
+			
+			int res = tps((visited | (1 << i)), i) + dist[city][i];
+			dp[visited][city] = Math.min(res, dp[visited][city]);
 		}
+		
 		return dp[visited][city];
 	}
 }
