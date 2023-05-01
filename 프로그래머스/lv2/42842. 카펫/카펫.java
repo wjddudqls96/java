568cdf016ebb2e4191336ed23713a000dd5453e2
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-05-01 12:02:05 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        
+        // 1. 약수 구하기
+        int size = brown + yellow;
+        
+        for(int i = 3; i < size; i++){
+            int j = size / i;
+            
+            if(size % i == 0 && j >= 3){
+                int col = Math.max(i, j);
+                int row = Math.min(i, j);
+                
+                if(yellow == (col - 2) * (row - 2)){
+                    answer[0] = col;
+                    answer[1] = row;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
