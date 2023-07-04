faaaee396393d56051ba4d8ec5354d29084d9f3b
정영빈 <59672589+jybin96@users.noreply.github.com>
2023-07-04 21:07:49 +0900

    [level 2] Title: 택배상자, Time: 151.32 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131704. 택배상자/택배상자.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] order) {
+        int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+        int orderIdx = 1;
+        int start = 0;
+        
+        while(true){
+            if(!stack.isEmpty() && order[start] == stack.peek()){
+                answer++;
+                start++;
+                stack.pop();
+                continue;
+            }
+            
+            if(orderIdx > order.length) break;
+            
+            if(order[start] == orderIdx){
+                answer++;
+                start++;
+                orderIdx++;
+                
+                continue;
+            }
+            
+            stack.push(orderIdx);
+            orderIdx++;
+        }
+        
+        return answer;
+    }
+}
