ccff2421df9f795398d68b319ec6bef72413184c
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2023-08-28 22:32:01 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 12.23 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -3,15 +3,20 @@ import java.util.*;
 class Solution {
     public String solution(String s) {
         String answer = "";
-        String[] split = s.split(" ");
+        
+        // 1. 삽입과 삭제가 logN, 마지막 원소 탐색할 경우 logN -> NlogN + logN
+        // 2. 즉 ArrayList를 만든뒤 (1) Collections.sort를 이용하면 NlogN, 첫번째 (1), 마지막 (1) -> NlogN
         TreeMap<Integer, Integer> map = new TreeMap<>();
         
+        String[] split = s.split(" ");
+        
         for(String str : split){
-            int num = Integer.parseInt(str);
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            map.put(Integer.parseInt(str), 0);
         }
         
         answer = map.firstKey() + " " + map.lastKey();
+        
+        // 즉 데이터가 하나 들어오고 정렬하고 또 들어오고 정렬하는 방식은 TreeMap 그렇지 않은 경우는 sort함수 사용해서 풀이!
         return answer;
     }
 }
