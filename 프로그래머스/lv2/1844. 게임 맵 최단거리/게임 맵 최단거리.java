02a804e07ac0e5b857e0545ff4aa643c58356b24
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2023-08-31 22:17:36 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.18 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/lv2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,59 @@
 import java.util.*;
 
-class Pos{
+class Pos {
     int x;
     int y;
-    int cnt;
+    int count;
     
-    Pos(int x, int y, int cnt){
+    public Pos(int x, int y, int count){
         this.x = x;
         this.y = y;
-        this.cnt = cnt;
+        this.count = count;
     }
 }
 
-
 class Solution {
-    static int[][] directions = {{1, 0},{-1, 0},{0, 1},{0, -1}};
-    static int result = -1;
+    int N, M;
     public int solution(int[][] maps) {
-        bfs(maps);
-        return result;
+        N = maps.length;
+        M = maps[0].length;
+        boolean[][] visited = new boolean[N][M];
+        
+        Pos pos = bfs(0, 0, visited, maps);
+        
+        return pos.count;
     }
     
-    static void bfs(int[][] maps){
-        int n = maps.length;
-        int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
-        Queue<Pos> queue = new ArrayDeque<>();
-        
-        visited[0][0] = true;
-        queue.offer(new Pos(0, 0, 1));
+    
+    public Pos bfs(int startX, int startY, boolean[][] visited, int[][] maps){
+        int[][] direction = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        Queue<Pos> queue = new ArrayDeque();
+        queue.offer(new Pos(startX, startY, 0));
+        visited[startY][startX] = true;
         
         while(!queue.isEmpty()){
             Pos cur = queue.poll();
             
-            if(cur.x == m - 1 && cur.y == n - 1){
-                result = cur.cnt;
-                break;
+            if(cur.x == M - 1 && cur.y == N - 1){
+                cur.count++;
+                return cur;
             }
             
             for(int i = 0; i < 4; i++){
-                int nextX = cur.x + directions[i][0];
-                int nextY = cur.y + directions[i][1];
+                int nextX = cur.x + direction[i][0];
+                int nextY = cur.y + direction[i][1];
                 
-                if(nextX < 0 || nextX >= m || nextY < 0 || nextY >= n) continue;
+                if(nextX < 0 || nextX >= M || nextY < 0 || nextY >= N) continue;
                 
-                if(maps[nextY][nextX] == 0 || visited[nextY][nextX]) continue;
+                if(visited[nextY][nextX]) continue;
+                
+                if(maps[nextY][nextX] == 0) continue;
                 
                 visited[nextY][nextX] = true;
-                queue.offer(new Pos(nextX, nextY, cur.cnt + 1));
+                queue.offer(new Pos(nextX, nextY, cur.count + 1));
             }
         }
+        
+        return new Pos(-1, -1, -1);
     }
 }
