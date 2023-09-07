0c71793b9b0887d64a3ecdf16b55667f2f9b6550
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2023-09-07 22:25:18 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.37 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/README.md
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/README.md
@@ -1,10 +1,10 @@
 # [level 2] 다리를 지나는 트럭 - 42583 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42583) 
 
 ### 성능 요약
 
-메모리: 74.4 MB, 시간: 0.40 ms
+메모리: 75.3 MB, 시간: 0.37 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/lv2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,71 +1,51 @@
 import java.util.*;
 
-class Truck{
+class Truck {
     int weight;
     int time;
     
-    public Truck(int weight, int time){
+    public Truck(int weight, int time) {
         this.weight = weight;
         this.time = time;
-    }
-    
-    @Override
-    public String toString(){
-        return weight + " " + time;
     }
 }
 
 class Solution {
-    static Queue<Truck> bridge;
-    static List<Truck> onLoad;
-    static int startIndex, w;
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        bridge = new ArrayDeque<>();
-        onLoad = new ArrayList<>();
+        Queue<Truck> queue = new ArrayDeque<>();
         
-        int cnt = 0;
+        int time = 0;
+        int index = 0;
+        int bridgeWeight = 0;
+        
         while(true){
-            if(bridge.isEmpty() && startIndex == truck_weights.length) break;
+            if(queue.isEmpty() && truck_weights.length == index) break;
             
-            // 1. 다리를 지날 수 있는지 확인하고 옮긴다.
-            while(!bridge.isEmpty() && bridge.peek().time == 0){
-                Truck t = bridge.poll();
-                w -= t.weight;
-                onLoad.add(t);
+            // 1.다리를 지날 수 있다면 지나간다.
+            if(!queue.isEmpty() && queue.peek().time == 0){
+                Truck truck = queue.poll();
+                bridgeWeight -= truck.weight;
             }
-            // 2. 트럭을 다리에 올릴 수 있는지 확인하고 옮긴다.
-            onBridge(truck_weights, weight, bridge_length);
-            // 3. 트럭의 시간을 1 줄인다.
-            for(Truck t : bridge){
-                t.time--;
+            
+            // 2. 만약 다리에 올릴 수 있다면. 다리에 올린다.
+            if(truck_weights.length > index) {
+                int cur = truck_weights[index];
+            
+                if(bridgeWeight + cur <= weight){
+                    queue.offer(new Truck(cur, bridge_length));
+                    bridgeWeight += cur;
+                    index++;
+                }
             }
-            cnt++;
+
+            // 3. 다리위의 트럭 1 감소
+            for(Truck truck : queue){
+                truck.time--;
+            }
+            
+            time++;
         }
         
-       answer = cnt;
-        
-        return answer;
-    }
-    
-    static void onBridge(int[] truck_weights, int weight, int bridge_length){
-        if(startIndex == truck_weights.length) return;
-        
-        int tweight = truck_weights[startIndex];
-        
-        if(!isPossibleOnBridge(tweight, weight)) return;  
-        
-        Truck t = new Truck(tweight, bridge_length);
-        bridge.offer(t);
-        w += tweight;
-        startIndex += 1;
-    }
-    
-    // 다리에 올릴 수 있는지 확인해야된다.
-    static boolean isPossibleOnBridge(int tweight, int weight){
-        if(tweight + w <= weight){
-            return true;
-        }
-        return false;
+        return time;
     }
 }
