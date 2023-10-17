98fd5a6f8665a976db28dc102c319791b87ea73e
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2023-10-17 22:22:25 +0900

    [Silver V] Title: 올림픽, Time: 152 ms, Memory: 14784 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -3,6 +3,7 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.StringTokenizer;
@@ -12,6 +13,7 @@ class Country implements Comparable<Country>{
 	int gold;
 	int silver;
 	int dong;
+	int rate;
 	
 	public Country(int num, int gold, int silver, int dong) {
 		this.num = num;
@@ -21,30 +23,19 @@ class Country implements Comparable<Country>{
 	}
 	
 	@Override
-	public int compareTo(Country country) {
+	public int compareTo(Country o) {
 		
-		if(this.gold > country.gold) {
-			return -1;
-		}
-		else if(this.gold == country.gold) {
-			if(this.silver > country.silver) {
-				return -1;
-			}
-			else if(this.silver == country.silver) {
-				if(this.dong > country.dong) {
-					return -1;
-				}
-				else {
-					return 1;
-				}
-			}
-			else {
-				return 1;
-			}
-		}
-		else {
-			return 1;
-		}
+		if(this.gold == o.gold) {
+            if(this.silver == o.silver) {
+                return o.dong- this.dong;
+            }
+            else {
+                return o.silver - this.silver;
+            }
+        }
+        else {
+            return o.gold - this.gold;
+        }
 	}
 	
 	@Override
@@ -56,12 +47,13 @@ class Country implements Comparable<Country>{
 public class Main {
     
 	public static void main(String[] args) throws Exception {
+    	
     	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     	StringBuilder sb = new StringBuilder();
     	StringTokenizer st = new StringTokenizer(in.readLine());
     	int N = Integer.parseInt(st.nextToken());
     	int K = Integer.parseInt(st.nextToken());
-    	PriorityQueue<Country> pq = new PriorityQueue<>();
+    	List<Country> list = new ArrayList<>();
     	
     	for(int i = 0; i < N; i++) {
     		st = new StringTokenizer(in.readLine());
@@ -71,32 +63,34 @@ public class Main {
     		int silver = Integer.parseInt(st.nextToken());
     		int dong = Integer.parseInt(st.nextToken());
     		
-    		pq.add(new Country(num, gold, silver, dong));
+    		list.add(new Country(num, gold, silver, dong));
+    	}
+    	
+    	Collections.sort(list);
+    	int index = 0;
+    	
+    	list.get(0).rate = 1;
+    	
+    	for(int i = 1; i < list.size(); i++) {
+    		int tempGold= list.get(i-1).gold;
+            int tempSilver = list.get(i-1).silver;
+            int tempDong = list.get(i-1).dong;
+            
+            Country now = list.get(i);
+            
+            if(now.num == K) {
+            	index = i;
+            }
+            
+            if(now.gold == tempGold && now.silver==tempSilver && now.dong==tempDong) {
+                list.get(i).rate = list.get(i-1).rate;
+            }
+            else {
+            	list.get(i).rate = i + 1;
+            }
     	}
     	
     	
-    	int result = 1;
-    
-    	
-    	Country temp = pq.poll();
-    	
-    	if(temp.num != K) {
-    		while(!pq.isEmpty()) {
-        		result++;
-        		
-        		Country country = pq.poll();
-        		
-        		if(K == country.num) {
-        			if(temp.dong == country.dong && temp.silver == country.silver && temp.gold == country.gold) {
-        				result--;
-        			}
-        			break;
-        		}
-        		
-        		temp = country;
-        	}
-    	}
-    	
-    	System.out.println(result);
+    	System.out.println(list.get(index).rate);
     }
 }
