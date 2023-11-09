fa7c723d6932f309252a1c17098ec1692b1cdf38
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2023-11-09 23:57:18 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 456 ms, Memory: 40608 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -0,0 +1,61 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    
+	public static void main(String[] args) throws Exception {
+    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	StringTokenizer st;
+    	
+    	st = new StringTokenizer(in.readLine());
+    	int N = Integer.parseInt(st.nextToken());
+    	int M = Integer.parseInt(st.nextToken());
+    	
+    	Map<Integer, Integer> map = new HashMap<>();
+    	st = new StringTokenizer(in.readLine());
+    	int[] arr = new int[N];
+    	int[] cnt = new int[100001];
+    	
+    	for(int i = 0; i < N; i++) {
+    		int num = Integer.parseInt(st.nextToken());
+    		arr[i] = num;
+    		map.put(num, 0);
+    	}
+    	
+    	int left = 0;
+    	int right = 0;
+    	int result = 0;
+    	
+    	while(left <= right) {
+    		
+    		if(right <= N - 1 && cnt[arr[right]] < M) {
+    			cnt[arr[right]]++;
+    			right++;
+    		}
+    		else if(cnt[arr[right]] == M) {
+    			cnt[arr[left]]--;
+    			left++;
+    		}
+    		
+    		result = Math.max(right - left, result);
+    		
+    		if(right == N) break;
+    	}
+    	
+    	System.out.println(result);
+    }
+}
