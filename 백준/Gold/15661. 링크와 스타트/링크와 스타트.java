40861f61259c34f3e98f4e4eb20b831c43b31ca5
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2024-01-02 11:23:05 +0900

    [Gold V] Title: 링크와 스타트, Time: 692 ms, Memory: 19076 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -15,65 +15,73 @@ import java.util.StringTokenizer;
 
 
 public class Main {
-	static int N, result;
-	static boolean[] visit;
-	static int[][] map;
-
-	public static void main(String[] args) throws Exception {
-    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    	StringBuilder sb = new StringBuilder();
-    	StringTokenizer st;
-    	
-    	N = Integer.parseInt(in.readLine());
-    	visit = new boolean[N];
-    	map = new int[N][N];
-    	result = Integer.MAX_VALUE;
-    	
-    	for(int i = 0; i < N; i++) {
-    		st = new StringTokenizer(in.readLine());
-    		
-    		for(int j = 0; j < N; j++) {
-    			map[i][j] = Integer.parseInt(st.nextToken());
-    		}
-    	}
-    	
-    	subSet(0, 0);
-    	
-    	System.out.println(result);
+   
+    static int [][] people;
+    static int N;
+    
+    static int answer;
+    static List<Integer> team1;
+    static List<Integer> team2;
+    
+    public static void main(String[] args) throws Exception  {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        N=Integer.parseInt(br.readLine());
+        
+        people=new int[N][N];
+        for(int i=0;i<N;i++) {
+            st=new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++) {
+                people[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+        team1=new ArrayList<>();
+        team2=new ArrayList<>();
+        answer=Integer.MAX_VALUE;
+        pickTeamWithBit();
+        System.out.println(answer);
+        
     }
-	
-	static void subSet(int count, int subSetCnt) {
-		
-		if(count == N) {
-			if(subSetCnt >= 1 && subSetCnt < N) {
-				calculation();
-			}
-			return;
-		}
-		
-		visit[count] = true;
-		subSet(count + 1, subSetCnt + 1);
-		visit[count] = false;
-		subSet(count + 1, subSetCnt);
-	}
-	
-	static void calculation() {
-		int startTeam = 0;
-		int linkTeam = 0;
-		
-		for(int i = 0; i < N; i++) {
-			
-			for(int j = i + 1; j < N; j++) {
-				
-				if(visit[i] && visit[j]) {
-					startTeam += map[i][j] + map[j][i];
-				}
-				else if(!visit[i] && !visit[j]) {
-					linkTeam += map[i][j] + map[j][i];
-				}
-			}
-		}
-		
-		result = Math.min(result, Math.abs(startTeam - linkTeam));
-	}
+    
+    static void pickTeamWithBit(){
+        for(int i=1;i<(1<<N)-1;i++) {
+            
+            for(int j=0;j<N;j++) {
+                if((i & (1<<j))!=0) {    //해당 j번째 비트가 켜져있을 경우
+                    team1.add(j);
+                }else {                    //해당 j번째 비트가 꺼져있을 경우
+                    team2.add(j);
+                }
+            }
+            findAnswer();
+            team1.clear();
+            team2.clear();
+        }
+    }
+    
+    static void findAnswer() {
+        int size1 =team1.size();
+        int size2 =team2.size();
+        
+        int sum1=0;
+        int sum2=0;
+        
+        for(int i=0;i<size1;i++) {
+            for(int j=0;j<size1;j++) {
+                if(i!=j) {
+                    sum1+=people[team1.get(i)][team1.get(j)];
+                }
+            }
+        }
+        for(int i=0;i<size2;i++) {
+            for(int j=0;j<size2;j++) {
+                if(i!=j) {
+                    sum2+=people[team2.get(i)][team2.get(j)];
+                }
+            }
+        }
+        
+        answer=Math.min(answer, Math.abs(sum1-sum2));
+        
+    }
 }
