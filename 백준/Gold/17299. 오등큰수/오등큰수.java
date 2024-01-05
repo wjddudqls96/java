5d75ba60ca662d9d9ff9bc959b9f0ab432724eb0
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2024-01-05 14:05:34 +0900

    [Gold III] Title: 오등큰수, Time: 1012 ms, Memory: 142156 KB -BaekjoonHub

--- 백준/Gold/17299. 오등큰수/오등큰수.java
+++ 백준/Gold/17299. 오등큰수/오등큰수.java
@@ -1,94 +1,37 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
-
-class Box {
-	int index;
-	int count;
-	
-	public Box(int index, int count) {
-		this.index = index;
-		this.count = count;
-	}
-	
-	@Override
-	public String toString() {
-		return this.index + " " + count;
-	}
-}
-
+import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int N;
-    static int[] arr;
-    
-	public static void main(String[] args) throws Exception {
-    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    	StringBuilder sb = new StringBuilder();
-    	StringTokenizer st;
-    	
-    	N = Integer.parseInt(in.readLine());
-    	arr = new int[N];
-    	Box[] boxes = new Box[N];
-    	Map<Integer, Integer> map = new HashMap<>();
-    	
-    	st = new StringTokenizer(in.readLine());
-    	
-    	for(int i = 0; i < N; i++) {
-    		int num = Integer.parseInt(st.nextToken()); 
-    		arr[i] = num;
-    		map.put(num, map.getOrDefault(num, 0) + 1);
-    	}
-    	
-    	for(int i = 0; i < N; i++) {
-    		boxes[i] = new Box(i, map.get(arr[i]));
-    	}
-    	
-    	// stack으로 값 구하기.
-    	int[] results = new int[N];
-    	
-    	Stack<Box> stack = new Stack<>();
-    	
-    	for(int i = 0; i < N; i++) {
-    		Box box = boxes[i];
-    		
-    		if(!stack.isEmpty() && stack.peek().count < box.count) {
-    			
-    			while(!stack.empty()) {
-    				
-    				if(stack.peek().count >= box.count) {
-    					break;
-    				};
-    				
-    				Box prevBox = stack.pop();
-    				results[prevBox.index] = arr[box.index];
-    			}
-    		}
-    		
-    		stack.push(box);
-    	}
-    	
-    	while(!stack.isEmpty()) {
-    		Box box = stack.pop();
-    		results[box.index] = -1;
-    	}
-    	
-    	for(int result : results) {
-    		sb.append(result + " ");
-    	}
-    	
-    	System.out.println(sb);
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuffer sb = new StringBuffer();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] data = new int[N];
+        int[] cnt = new int[1_000_001];
+        int[] answer = new int[N];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i<N; i++) {
+            data[i] = Integer.parseInt(st.nextToken());
+            cnt[data[i]]++;
+        }
+
+        Stack<Integer> s = new Stack<>();
+        for(int i = 0; i<N; i++){
+            while(!s.isEmpty() && cnt[data[s.peek()]]<cnt[data[i]]){
+                answer[s.pop()] = data[i];
+            }
+
+            s.add(i);
+        }
+
+        while(!s.isEmpty())
+            answer[s.pop()] = -1;
+
+        for(int i = 0; i<N; i++)
+            sb.append(answer[i]).append(" ");
+
+        System.out.println(sb);
     }
 }
