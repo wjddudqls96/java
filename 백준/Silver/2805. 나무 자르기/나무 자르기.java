21c33da44f371364d0e8d23f3a5cb0ebf46aa6cf
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2024-01-08 12:14:06 +0900

    [Silver II] Title: 나무 자르기, Time: 1128 ms, Memory: 119564 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -0,0 +1,73 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    static int N,M;
+    static int[] arr;
+    
+	public static void main(String[] args) throws Exception {
+    	
+    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    	StringBuilder sb = new StringBuilder();
+    	StringTokenizer st;
+    	
+    	st = new StringTokenizer(in.readLine());
+    	
+    	N = Integer.parseInt(st.nextToken());
+    	M = Integer.parseInt(st.nextToken());
+    	
+    	arr = new int[N];
+    	
+    	st = new StringTokenizer(in.readLine());
+    	
+    	for(int i = 0; i < N; i++) {
+    		arr[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	Arrays.sort(arr);
+    	
+    	int left = 1;
+    	int right = arr[arr.length - 1];
+    	int result = 0;
+    	
+    	while(left <= right) {
+    		int mid = (left + right) / 2;
+    		long sum = 0;
+    		
+    		for(int i = 0; i < N; i++) {
+    			
+    			long num = arr[i] - mid;
+    			
+    			if(num > 0) {
+    				sum += num;
+    			}
+    		}
+    		
+    		// 만약 나머지 길이들의 합이 원하는 값보다 크다면. 
+    		if(sum >= M) {
+    			result = Math.max(result, mid);
+    			left = mid + 1;
+    		}
+    		else {
+    			right = mid - 1;
+    		}
+    	}
+    	
+    	System.out.println(result);
+    }
+	
+}
