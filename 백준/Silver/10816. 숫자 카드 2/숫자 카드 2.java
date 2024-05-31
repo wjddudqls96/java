7f7f509b0db2f67457175625f3370c15e5d541b0
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2024-05-31 13:08:02 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1052 ms, Memory: 176596 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -1,85 +1,45 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Deque;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
-
 public class Main {
-    static int N;
-    static int[] arr;
-    
-	public static void main(String[] args) throws Exception {
-    	BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    	StringBuilder sb = new StringBuilder();
-    	StringTokenizer st;
-    	
-    	N = Integer.parseInt(in.readLine());
-    	arr = new int[N];
-    	st = new StringTokenizer(in.readLine());
-    	
-    	for(int i = 0; i < N; i++) {
-    		arr[i] = Integer.parseInt(st.nextToken());
-    	}
-    	
-    	Arrays.sort(arr);
-    	
-    	int count = Integer.parseInt(in.readLine());
-    	st = new StringTokenizer(in.readLine());
-    	
-    	for(int i = 0; i < count; i++) {
-    		int num = Integer.parseInt(st.nextToken());
-    		
-    		sb.append(getUpperBound(num) - getLowerBound(num) + " ");
-    	}
-    	
-    	System.out.println(sb);
+	
+    public static void main(String[] args) throws Exception {
+    	//System.setIn(new FileInputStream("./input.txt"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = null;
+        
+        int N = Integer.parseInt(in.readLine());
+        
+        st = new StringTokenizer(in.readLine());
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < N; i++) {
+        	int num = Integer.parseInt(st.nextToken());
+        	
+        	map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        
+        int M = Integer.parseInt(in.readLine());
+        
+        st = new StringTokenizer(in.readLine());
+        sb = new StringBuilder();
+        
+        for(int i = 0; i < M; i++) {
+        	int num = Integer.parseInt(st.nextToken());
+        	
+        	if(map.get(num) != null) {
+        		sb.append(map.get(num)).append(" ");
+        	}
+        	else {
+        		sb.append(0).append(" ");
+        	}
+        }
+        
+        System.out.println(sb);
     }
-	
-	static int getLowerBound(int key) {
-		int low = 0;
-		int high = arr.length;
-		
-		while(low < high) {
-			int mid = (low + high) / 2;
-			
-			if(key <= arr[mid]) {
-				high = mid;
-			}
-			else {
-				low = mid + 1;
-			}
-		}
-		
-		return low;
-	}
-	
-	static int getUpperBound(int key) {
-		int low = 0;
-		int high = arr.length;
-		
-		while(low < high) {
-			int mid = (low + high) / 2;
-			
-			if(key < arr[mid]) {
-				high = mid;
-			}
-			else {
-				low = mid + 1;
-			}
-		}
-		
-		return low;
-	}
-	
 }
