943ea4c8c20316afca0a47bb3b9c6a52520800ee
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2024-05-31 12:47:28 +0900

    [Silver IV] Title: 수 찾기, Time: 612 ms, Memory: 43464 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,55 +1,74 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		
-		int N = Integer.parseInt(in.readLine());
-		int[] arr = new int[N];
-		
-		String[] split1 = in.readLine().split(" ");
-		for(int i = 0; i < N; i++) {
-			arr[i] = Integer.parseInt(split1[i]);
-		}
-		
-		int M = Integer.parseInt(in.readLine());
-		int[] arr2 = new int[M];
-		
-		String[] split2 = in.readLine().split(" ");
-		for(int i = 0; i < M; i++) {
-			arr2[i] = Integer.parseInt(split2[i]);
-		}
-		
-		Arrays.sort(arr);
-		
-		for(int num : arr2) {
-			int result = binarySearch(num, 0, arr.length - 1, arr);
-			sb.append(result).append("\n");
-		}
-		
-		System.out.println(sb);
-		
-		
-	}
+	private static int N;
+	private static long[] arr;
 	
-	static int binarySearch(int key, int start, int end, int[] arr) {
-		while(start <= end) {
-			int mid = (end + start) / 2;
-			
-			if(key > arr[mid]) {
-				start = mid + 1;
-			}
-			else if(key < arr[mid]) {
-				end = mid - 1;
-			}
-			else if(key == arr[mid]){
-				return 1;
-			}
-		}
-		return 0;
-	}
+    public static void main(String[] args) throws Exception {
+    	//System.setIn(new FileInputStream("./input.txt"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        
+        N = Integer.parseInt(in.readLine());
+        
+        arr = new long[N];
+        
+        st = new StringTokenizer(in.readLine());
+        
+        for(int i = 0; i < N; i++) {
+        	arr[i] = Long.parseLong(st.nextToken());
+        }
+        
+        // 이분 탐색 시작
+        
+        // 1. 정렬
+        Arrays.sort(arr);
+        
+        int M = Integer.parseInt(in.readLine());
+        
+        st = new StringTokenizer(in.readLine());
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < M; i++) {
+        	boolean result = isFind(Long.parseLong(st.nextToken()));
+        	
+        	if(result) {
+        		sb.append("1").append("\n");
+        	}
+        	else {
+        		sb.append("0").append("\n");
+        	}
+        }
+        
+        System.out.println(sb);
+    }
+    
+    static boolean isFind(long target) {
+    	
+    	int left = 0;
+        int right = N - 1;
+        boolean isFind = false;
+        
+        while(left <= right) {
+        	int mid = (left + right) / 2;
+        	
+        	if(arr[mid] > target) {
+        		right = mid - 1;
+        	}
+        	else if(arr[mid] < target) {
+        		left = mid + 1;
+        	}
+        	else {
+        		isFind = true;
+        		break;
+        	}
+        }
+        
+        return isFind;
+    }
 }
