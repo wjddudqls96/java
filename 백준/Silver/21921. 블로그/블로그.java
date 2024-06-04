0d6862d7f6852ce2bd6b045753c62466c7e4d3d9
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2024-06-04 16:27:13 +0900

    [Silver III] Title: 블로그, Time: 360 ms, Memory: 35060 KB -BaekjoonHub

--- 백준/Silver/21921. 블로그/블로그.java
+++ 백준/Silver/21921. 블로그/블로그.java
@@ -1,65 +1,68 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		st = new StringTokenizer(in.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int X = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(in.readLine());
-		
-		int[] arr = new int[N + 1];
-		
-		for(int i = 1; i <= N; i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		
-		int temp = 0;
-		int start = 1;
-		int result = 0;
-		int count = 0;
-		
-		for(int i = 1; i <= N; i++) {
-			if(i <= X) {
-				temp += arr[i];
-			}
-			else {
-				temp += (arr[i] - arr[start]);
-				start++;
-			}
-			
-			
-			if(result < temp) {
-				result = temp;
-				count = 1;
-			}
-			else if(result == temp){
-				count++;
-			}
-		}
-		
-		
-		if(result == 0) {
-			System.out.println("SAD");
-		}
-		else {
-			System.out.println(result);
-			System.out.println(count);
-		}
-	}
+	
+    public static void main(String[] args) throws Exception {
+    	//System.setIn(new FileInputStream("./input.txt"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = null;
+        
+        st = new StringTokenizer(in.readLine());
+        
+        int N = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
+        
+        st = new StringTokenizer(in.readLine());
+        
+        int[] arr = new int[N + 1];
+        int total = 0;
+        
+        for(int i = 1; i <= N; i++) {
+        	int num = Integer.parseInt(st.nextToken());
+        	arr[i] = num;
+        	
+        	if(i <= X) {
+        		total += num;
+        	}
+        }
+        
+        int MAX = total;
+        int count = 0;
+        
+        if(MAX != 0) {
+        	count++;
+        }
+        
+        for(int i = 2; i <= N - X + 1; i++) {
+        	
+        	total -= arr[i - 1];
+        	total += arr[i + X - 1];
+        	
+        	if(MAX < total) {
+        		count = 1;
+        		MAX = total;
+        	}
+        	else if(MAX == total){
+        		count++;
+        	}
+        }
+        
+        
+        
+        if(MAX == 0) {
+        	System.out.println("SAD");
+        }
+        else {
+        	System.out.println(MAX);
+            System.out.println(count);
+        }
+        
+        
+    }
 }
