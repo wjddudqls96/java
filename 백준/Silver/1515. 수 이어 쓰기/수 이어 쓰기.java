c96e89e3b3d3374da6fc51bfbae45c37bf290754
정영빈 <59672589+wjddudqls96@users.noreply.github.com>
2024-06-05 14:49:40 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 144 ms, Memory: 16204 KB -BaekjoonHub

--- 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -1,46 +1,45 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		String line = in.readLine();
-		
-		int num = 0;
-		int index = 0;
-		
-		while (true) {
-			num++;
-			String numStr = String.valueOf(num);
-			boolean flag = false;
-			
-			for(int i = 0; i < numStr.length(); i++) {
-				if(numStr.charAt(i) == line.charAt(index)) {
-					index++;
-				}
-				
-				if(index == line.length()) {
-					flag = true;
-					break;
-				}
-			}
-			
-			if(flag) break;
-		}
-		
-		System.out.println(num);
-	}
+	
+    public static void main(String[] args) throws Exception {
+    	//System.setIn(new FileInputStream("./input.txt"));
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = null;
+        StringBuilder sb = null;
+        
+        // 234092
+        
+        String str = in.readLine();
+        int num = 0;
+        int index = 0;
+        boolean flag = true;
+        
+        while(flag) {
+        	num++;
+        	
+        	String numStr = Integer.toString(num);
+        	
+        	for(int i = 0; i < numStr.length(); i++) {
+        		char c = numStr.charAt(i);
+        		
+        		if(c == str.charAt(index)) {
+        			index++;
+        		}
+        		
+        		if(index == str.length()) {
+        			flag = false;
+        			break;
+        		}
+        	}
+        }
+        
+        System.out.println(num);
+        
+    }
 }
